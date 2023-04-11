4b04e11337a082ff8bd0780b0afce413fef60c24
hthnh <98790313+hthnh@users.noreply.github.com>
2023-04-11 14:43:35 +0700

    Update FIlterFood.cpp

--- FIlterFood.cpp
+++ FIlterFood.cpp
@@ -4,8 +4,7 @@
 char AllFood[100] = "D:\\code\\KHKT-Order\\InOut\\AllFood.txt";
 char WeeklyLog[100] = "D:\\code\\KHKT-Order\\InOut\\WeeklyLog.txt";
 char FoodAfterFilter[100] = "D:\\code\\KHKT-Order\\InOut\\FoodAfterFilter.txt";
-struct Food
-{
+struct Food{
     int ID;
     char Name[50];
     int PickTime;
@@ -22,7 +21,7 @@ void importFood(){
         fscanf(f,"%d",&F[i].PickTime);
         fscanf(f,"%d",&F[i].LastPick);
         fscanf(f,"%d",&F[i].Calories);
-        if(F[i].ID >= 999998) break;
+        if(F[i].ID == 0) break;
         i++;
     }  
     fclose(f);
@@ -37,7 +36,7 @@ void importFoodLog(){
         fscanf(f,"%d",&FF[i].PickTime);
         fscanf(f,"%d",&FF[i].LastPick);
         fscanf(f,"%d",&FF[i].Calories);
-        if(FF[i].ID >= 999998) break;
+        if(FF[i].ID == 0) break;
         i++;
     }  
     fclose(f);
@@ -49,7 +48,7 @@ void clearFood(int i){
         F[i].PickTime = F[i+1].PickTime;
         F[i].LastPick = F[i+1].LastPick;
         F[i].Calories = F[i+1].Calories;
-        if(F[i+1].ID >= 999998) break;
+        if(F[i+1].ID == 0) break;
         i++;
     }
 }
@@ -58,10 +57,6 @@ void OutFood(){
     FILE *f = fopen(FoodAfterFilter,"w");
     int i = 0;
     while(1){
-        if(F[i].ID >= 999998){
-            fprintf(f,"999999");
-            break;
-        }
         fprintf(f,"%d\n%s\n%d\n%d\n%d\n",F[i].ID,F[i].Name,F[i].PickTime,F[i].LastPick,F[i].Calories );
         i++;
     }
@@ -87,10 +82,10 @@ int main(){
                 clearFood(i);
             }
             
-            if(F[y].ID >= 999998) break;
+            if(F[y].ID == 0) break;
             y++;
         }
-        if(F[i].ID >= 999998) break;
+        if(F[i].ID == 0) break;
         i++;
     }
     
