195a3708ed790cc7d9b0f278dfcff4109f689278
hthnh <haithinh09032006@gmail.com>
2023-04-13 15:05:47 +0700

    update MainProgram.py

--- MainProgram.py
+++ MainProgram.py
@@ -1,14 +1,24 @@
-from kivy.uix.button import Button
 from kivy.app import App
-from functools import partial
-class KivyButton(App):
-    def disable(self, instance, *args):
-        instance.disabled = True
-    def update(self, instance, *args):
-        instance.text = "I am Disabled!"
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivymd.uix.datatables import MDDataTable
+from kivy.metrics import dp
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.datatables import MDDataTable
+
+class MyApp(MDApp):
     def build(self):
-        mybtn = Button(text="Click me to disable")
-        mybtn.bind(on_press=partial(self.disable, mybtn))
-        mybtn.bind(on_press=partial(self.update, mybtn))
-        return mybtn
-KivyButton().run()
+        layout = Screen()
+        layout = BoxLayout(orientation='vertical')
+        table = MDDataTable(column_data=[
+            ("Breakfast", dp(30)),
+            ("Lunch", dp(30)),
+            ("Dinner", dp(30))
+        ])
+        layout.add_widget(table)
+        return layout
+
+
+MyApp().run()
