261b2b12069e334979e7ff146bd8b7894cfa0c18
hthnh <haithinh09032006@gmail.com>
2023-04-13 12:25:18 +0700

    Update MainProgram.py

--- MainProgram.py
+++ MainProgram.py
@@ -1,5 +1,14 @@
-import subprocess 
-import tkinter as tk
-subprocess.call(["d:\code\KHKT-Order\clearTotalCalories.exe"])
-
-
+from kivy.uix.button import Button
+from kivy.app import App
+from functools import partial
+class KivyButton(App):
+    def disable(self, instance, *args):
+        instance.disabled = True
+    def update(self, instance, *args):
+        instance.text = "I am Disabled!"
+    def build(self):
+        mybtn = Button(text="Click me to disable")
+        mybtn.bind(on_press=partial(self.disable, mybtn))
+        mybtn.bind(on_press=partial(self.update, mybtn))
+        return mybtn
+KivyButton().run()
