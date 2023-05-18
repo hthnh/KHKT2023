4b4af1d399197be2cbb9501d36e9d543705f3798
hthnh <haithinh09032006@gmail.com>
2023-05-18 17:20:52 +0700

    update

--- TheMenuApp.py
+++ TheMenuApp.py
@@ -3,12 +3,19 @@ from kivymd.uix.screen import Screen
 from kivymd.uix.datatables import MDDataTable
 from kivy.metrics import dp
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
 
 
 class MenuApp(MDApp):
     def build(self):
         screen = Screen()
-
+        mainProgram = BoxLayout(orientation = "horizontal")
+        AllFunc = BoxLayout(orientation = "vertical")
+        
+        
+        
         table = MDDataTable(
             column_data = [
                 ("date", dp(20)),
@@ -27,17 +34,57 @@ class MenuApp(MDApp):
             ]
         )
 
-        btnAddUser = Button(text = "Add User")
-        btnOrder = Button(text = "Order")
-        btnPickNow = Button(text = "Pick Now")
-        btnNutrition = Button(text = "Nutrition")
 
-        
 
-        screen.add_widget(table)
+        labAddUser = Label(text = "Add User",
+                           background_color = (1,1,1,1),
+                            size_hint = (.5,.5),
+                            pos_hint = {"center_x": .5},
+                            font_size = dp(20))
+        btnAddUser = Button(background_color =(0,0,0,0),
+                            size_hint = (.5,.5),
+                            pos_hint = {"center_x": .5})
+        btnAddUser.bind(on_press = self.test)
+
+        btnOrder = Button(text = "Order",
+                            background_color =(0,249,255,1.000),
+                            size_hint = (.5,.5),
+                            pos_hint = {"center_x": .5})
+        btnOrder.bind(on_press = self.test)
+
+        btnPickNow = Button(text = "Pick Now",
+                            background_color =(0,249,255,1.000),
+                            size_hint = (.5,.5),
+                            pos_hint = {"center_x": .5})
+        btnPickNow.bind(on_press = self.test)
+
+        btnNutrition = Button(text = "Nutrition",
+                            background_color =(0,249,255,1.000),
+                            size_hint = (.5,.5),
+                            pos_hint = {"center_x": .5})
+        btnNutrition.bind(on_press = self.test)
+
+        AllFunc.add_widget(labAddUser)
+        AllFunc.add_widget(btnAddUser)
+        AllFunc.add_widget(btnOrder)
+        AllFunc.add_widget(btnPickNow)
+        AllFunc.add_widget(btnNutrition)
+
+
+
+
+
+        mainProgram.add_widget(AllFunc)
+        mainProgram.add_widget(table)
+        screen.add_widget(mainProgram)
 
         return screen
     
+
+
+    def test(self , event):
+        print("test")
+ 
 
 
 MenuApp().run()
