8f1aa0577158de3e1be57874092551619db42485
hthnh <haithinh09032006@gmail.com>
2023-07-03 16:21:07 +0700

    jkjda

---
+++ Core/ImportFile.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    FILE *f;
+    f = fopen("InOut\\conditionActivate.txt","w");
+    fprintf(f,"");
+    fclose(f);
+    f = fopen("InOut\\DailyFood.txt","w");
+    fprintf(f,"");
+    fclose(f);
+    f = fopen("InOut\\deleteID.txt","w");
+    fprintf(f,"");
+    fclose(f);
+    f = fopen("InOut\\FoodAfterFilter.txt","w");
+    fprintf(f,"");
+    fclose(f);
+    f = fopen("InOut\\heightWeight.txt","w");
+    fprintf(f,"");
+    fclose(f);
+    f = fopen("InOut\\IDfood.txt","w");
+    fprintf(f,"");
+    fclose(f);
+    f = fopen("InOut\\TotalCalories.txt","w");
+    fprintf(f,"");
+    fclose(f);
+    f = fopen("InOut\\WeeklyLog.txt","w");
+    fprintf(f,"");
+    fclose(f);
+}
